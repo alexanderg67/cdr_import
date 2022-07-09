aeb9f5945063133ca9d64540763b937db0d02887
Alexander <you@gmail.com>
2022-07-09 14:42:40 +0300

    second

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const lineByLine = require('n-readlines');
 const fs = require('fs')
 const sql = require('mssql')
 var path = require('path');
-let CDR_DIR='C:\\Users\\Alex\\cdr-import\\'
-let ARCHIVE_DIR='C:\\Users\\Alex\\cdr-import\\arch\\'
+const { dirname } = require('path');
+const logger = require('logger').createLogger('cdrService.log');
+logger.setLevel('debug');
+const DBNAME='cdr'
+let CDR_DIR='C:\\Users\\Alex\\Pictures\\cdr-import\\'
+let ARCHIVE_DIR='C:\\Users\\Alex\\Pictures\\cdr-import\\arch\\'
 const sqlConfig = {
     user: 'sa',
     password: '12345',
@@ -21,25 +25,39 @@ const sqlConfig = {
      
   }
   CDR_DIR= path.normalize(CDR_DIR);   
-  ARCHIVE_DIR= path.normalize(ARCHIVE_DIR);   
+  ARCHIVE_DIR= path.normalize(ARCHIVE_DIR);
+     
  async function  main() {
+  
+  try {
   const pool= await sql.connect(sqlConfig)
+  
+   
+  logger.info('SQL:Successfully connected to  ',sqlConfig.server);
   await new Promise(resolve => setTimeout(resolve, 5000)); //set delay for sql connect
-  const dirarray=fs.readdirSync(CDR_DIR).filter ( filename =>  {
+  const dirArray=fs.readdirSync(CDR_DIR).filter ( filename =>  {
     return  filename.startsWith("cdr_")  
   } )
-    console.log('files:',dirarray)
-    //got files
-    dirarray.forEach( item => {
+  if (dirArray.length===0) {
+    logger.debug('No cdr files. Exiting');
+    process.exit(0)
+  }
+    //console.log('files:',dirArray)
+    logger.debug('CDR file list:', dirArray);
+    
+    dirArray.forEach( item => {
+       
+       
       const liner = new lineByLine(CDR_DIR+item);
-      console.log(item)
+      logger.debug('Reading file:',item);
       let line;
       let lineNumber = 0;
       while (line = liner.next()) {
-        if(lineNumber===0 || lineNumber===1) {
+        if(lineNumber===0 || lineNumber===1) { // skip 1 and 2 lines with fields name and types
           lineNumber++;
           continue 
         }
+         
          line=line.toString('utf8');
          let arr=line.split(',')
          let cdrRecordType,globalCallID_callManagerId,globalCallID_callId,origLegCallIdentifier,dateTimeOrigination,origNodeId,origSpan,origIpAddr,callingPartyNumber
@@ -52,29 +70,50 @@ const sqlConfig = {
          origSpan=arr[6]
          origIpAddr=arr[7]
          callingPartyNumber=arr[8]
-         let query=`insert into cdr   values (${cdrRecordType},${globalCallID_callManagerId},${globalCallID_callId},${origLegCallIdentifier}, ${dateTimeOrigination},${origNodeId},${origSpan},${origIpAddr},${callingPartyNumber})`
-        console.log(query)
+         let query=`insert into ${DBNAME}   values (${cdrRecordType},${globalCallID_callManagerId},${globalCallID_callId},${origLegCallIdentifier}, ${dateTimeOrigination},${origNodeId},${origSpan},${origIpAddr},${callingPartyNumber})`
+        //console.log(query)
         let insertRow= async function(query) {
+         try {
           let result1 = await  pool.request().query(query)
+         }catch(err) {
+           
+            logger.debug(`SQL Insert Error: ${err.name} ${err.message}`);
+            process.exit(1)
+         } 
+           
         }
+        
          insertRow(query)
          lineNumber++;
     }
-    
-    console.log('end of  file',item);
+    logger.debug(`File ${item} handled`);
+      
     })
-
-    dirarray.forEach( item => { 
+    await new Promise(resolve => setTimeout(resolve, 1500));
+    logger.debug('Moving CDR  files to Archive  folder');
+    dirArray.forEach( item => { 
       fs.rename(CDR_DIR+item, ARCHIVE_DIR+item , function (err) {
-        if (!err) { console.log('moved',item ) }
+        if (err) {
+          logger.debug(`Error moving file ${item}`);
+           
+       }else {
+          logger.debug(`File ${item} was moved`);
+       }
       })
     })
+    await new Promise(resolve => setTimeout(resolve, 2000));  
+    logger.debug('All Files are handled');
     
-
+  }catch(err) {
+    logger.info('Critical error:',err);
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    process.exit(1)
+  }
  }
   
 
  main()
+
  
   /*
 
